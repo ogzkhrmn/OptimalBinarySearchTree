3cbf8156e85845c62aff2eeb2c28b14e6485d30f
okahraman <oguz.kahraman@adesso.com.tr>
2019-04-11 18:59:24 +0300

    added double and getting element

--- DynamicProject/dynamic.c
+++ DynamicProject/dynamic.c
@@ -3,24 +3,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void allocateArray(int **arr, int length) {
+void allocateArray(double **arr, int length) {
 	int i;
-	for ( i = 0; i <= length; i++)
+	for (i = 0; i <= length; i++)
+	{
+		arr[i] = (double*)calloc(length, sizeof(double));
+	}
+}
+
+void allocateArrayInt(int **arr, int length) {
+	int i;
+	for (i = 0; i <= length; i++)
 	{
 		arr[i] = (int*)calloc(length, sizeof(int));
 	}
 }
 
-void calculate(int** mainArray, int* keyArray, int** rootArray, int i, int j) {
-	int k, newWeight;
-	int totalPriority = 0;
-	int weight = INT_MAX;
+void calculate(double** mainArray, double* keyArray, int** rootArray, int i, int j) {
+	int k;
+	double newWeight;
+	double totalPriority = 0;
+	double weight = INT_MAX;
 	int position = 0;
 	for (k = i; k < j; k++) {
 		totalPriority += keyArray[k];
 	}
 	for (k = i; k < j; k++) {
-		newWeight = mainArray[i][k] + mainArray[k+1][j] + totalPriority;
+		newWeight = mainArray[i][k] + mainArray[k + 1][j] + totalPriority;
 		if (newWeight < weight) {
 			weight = newWeight;
 			position = k;
@@ -34,29 +43,35 @@ void createPrintArray(int** rootArray, int** printArray, int left, int right, in
 	if (left < size && right < size && right >= 0 && left >= 0) {
 		if (rootArray[left][right] != -1) {
 			printArray[step][right] = rootArray[left][right];
-			createPrintArray(rootArray, printArray, rootArray[left][right] +1, right, size, step + 1);
+			createPrintArray(rootArray, printArray, rootArray[left][right] + 1, right, size, step + 1);
 			createPrintArray(rootArray, printArray, left, rootArray[left][right], size, step + 1);
 		}
-		
+
 	}
 }
 
 int main() {
-
+	//start initializing to variables.
 	int i, j, keyNumber;
+	printf("Eleman sayisini giriniz : ");
 	scanf("%d", &keyNumber);
 
-	int *pKey, **mainArray, **rootArray, **printArray;
+	double *pKey, **mainArray;
+	char *pKeyValue;
+	int **rootArray, **printArray, writedElement = 0;
 
-	pKey = (int*)malloc(keyNumber * sizeof(int));
-	mainArray = (int**)calloc(keyNumber, sizeof(int*));
+	pKey = (double*)malloc(keyNumber * sizeof(double));
+	pKeyValue = (char*)malloc(keyNumber * sizeof(char));
+	mainArray = (double**)calloc(keyNumber, sizeof(double*));
 	rootArray = (int**)calloc(keyNumber, sizeof(int*));
 	printArray = (int**)calloc(keyNumber, sizeof(int*));
 
 	allocateArray(mainArray, keyNumber);
-	allocateArray(rootArray, keyNumber);
-	allocateArray(printArray, keyNumber);
+	allocateArrayInt(rootArray, keyNumber);
+	allocateArrayInt(printArray, keyNumber);
+	//stop initializing to variables.
 
+	//initialize to arrays
 	for (i = 0; i <= keyNumber; i++) {
 		for (j = 0; j <= keyNumber; j++) {
 			printArray[i][j] = -1;
@@ -68,31 +83,39 @@ int main() {
 		}
 	}
 
-	for  (i = 0; i <= keyNumber; i++)
+	//read inputs from user
+	for (i = 0; i <= keyNumber; i++)
 	{
 		if (i < keyNumber) {
+			printf("\n%d elemani giriniz.", i);
+			scanf(" %c", &pKeyValue[i]);
 			printf("\n%d elemani için olasilik giriniz.", i);
-			scanf("%d", &pKey[i]);
+			scanf("%lf", &pKey[i]);
 		}
-		
+
 		mainArray[i][i] = 0;
-		mainArray[i][i+1] = pKey[i];
+		mainArray[i][i + 1] = pKey[i];
 		rootArray[i][i + 1] = i;
-		
+
 	}
 
+	//calculate costs.
 	for (i = 2; i <= keyNumber; i++) {
 		for (j = i - 2; j >= 0; j--) {
 			calculate(mainArray, pKey, rootArray, j, i);
 		}
 	}
 
+	//creating array for print to screen
 	createPrintArray(rootArray, printArray, 0, keyNumber, keyNumber + 1, 0);
 
-	for (i = 0; i <= keyNumber; i++) {
+	//print to screen
+	printf("Toplam masraf:%lf\n", mainArray[0][keyNumber]);
+	for (i = 0; i <= keyNumber && writedElement <= keyNumber; i++) {
 		for (j = 0; j <= keyNumber; j++) {
-			if (printArray[i][j] != -1 ) {
-				printf("%d", printArray[i][j]);
+			if (printArray[i][j] != -1) {
+				printf("%c", pKeyValue[printArray[i][j]]);
+				writedElement++;
 			}
 		}
 		printf("\n");
